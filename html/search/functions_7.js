90f45a387524445be38e5eb247b0034c8fd1ca56
Doxygen CI <ci-arduino@invalid>
2026-02-27 17:55:28 +0000

    Deploy docs to GitHub Pages from commit dba89f96cc
    Commit: dba89f96cc264e2199d85c7cb33e63ffae7223fa
    GitHub Actions run: 22497462976

--- html/search/functions_7.js
+++ html/search/functions_7.js
@@ -5,9 +5,9 @@ var searchData=
   ['setgpiomode',['setGPIOMode',['../class_adafruit___t_m_f8806.html#ad9e3800aa35e77d0452d036fa064815a',1,'Adafruit_TMF8806']]],
   ['setiterations',['setIterations',['../class_adafruit___t_m_f8806.html#ae7e5177fcb7fde066189893bb495a824',1,'Adafruit_TMF8806']]],
   ['setopticalconfig',['setOpticalConfig',['../class_adafruit___t_m_f8806.html#a19db21bf432b3c28b89ec79e3902a2ae',1,'Adafruit_TMF8806']]],
-  ['setrepetitionperiod',['setRepetitionPeriod',['../class_adafruit___t_m_f8806.html#a5248a33e553fa1edbf4f471b201f52e3',1,'Adafruit_TMF8806']]],
+  ['setrepetitionperiod_5fms',['setRepetitionPeriod_ms',['../class_adafruit___t_m_f8806.html#af4f551259d1d5a198b0abc6f1622e807',1,'Adafruit_TMF8806']]],
+  ['setsnrthreshold',['setSNRThreshold',['../class_adafruit___t_m_f8806.html#adb88d236850a57c6a33176a4d74c06e6',1,'Adafruit_TMF8806']]],
   ['setspaddeadtime',['setSpadDeadTime',['../class_adafruit___t_m_f8806.html#a3b35a3e4e96f842f04d1861d4db4eb68',1,'Adafruit_TMF8806']]],
-  ['setthreshold',['setThreshold',['../class_adafruit___t_m_f8806.html#a47bf9de236d9e64021a6bfbbc5ca145a',1,'Adafruit_TMF8806']]],
   ['startmeasuring',['startMeasuring',['../class_adafruit___t_m_f8806.html#a8d6aab9436d83a1e7a48ff1f8139713f',1,'Adafruit_TMF8806']]],
   ['stopmeasuring',['stopMeasuring',['../class_adafruit___t_m_f8806.html#a82784935ed5b3e6e9eddc3bf1ad06bb4',1,'Adafruit_TMF8806']]]
 ];
