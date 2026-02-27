5bfacc3caade33cc20aa65d6b52790a08c703421
Doxygen CI <ci-arduino@invalid>
2026-02-27 17:45:20 +0000

    Deploy docs to GitHub Pages from commit c7647ee5fd
    Commit: c7647ee5fd9f66e61c93710878e4618ace7193d1
    GitHub Actions run: 22497100077

--- html/menudata.js
+++ html/menudata.js
@@ -17,7 +17,16 @@ var menudata={children:[
 {text:"s",url:"functions.html#index_s"},
 {text:"t",url:"functions.html#index_t"},
 {text:"~",url:"functions.html#index_0x7e"}]},
-{text:"Functions",url:"functions_func.html"},
+{text:"Functions",url:"functions_func.html",children:[
+{text:"a",url:"functions_func.html#index_a"},
+{text:"b",url:"functions_func.html#index_b"},
+{text:"d",url:"functions_func.html#index_d"},
+{text:"e",url:"functions_func.html#index_e"},
+{text:"g",url:"functions_func.html#index_g"},
+{text:"p",url:"functions_func.html#index_p"},
+{text:"r",url:"functions_func.html#index_r"},
+{text:"s",url:"functions_func.html#index_s"},
+{text:"~",url:"functions_func.html#index_0x7e"}]},
 {text:"Variables",url:"functions_vars.html"}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
--- html/search/all_5.js
+++ html/search/all_5.js
@@ -2,7 +2,14 @@ var searchData=
 [
   ['getcalibrationdata',['getCalibrationData',['../class_adafruit___t_m_f8806.html#aed5178793ba66b360b91062709882a06',1,'Adafruit_TMF8806']]],
   ['getchipid',['getChipID',['../class_adafruit___t_m_f8806.html#ae1d7e2a8b17bd5024bf3d9db770d38d8',1,'Adafruit_TMF8806']]],
+  ['getdistancemode',['getDistanceMode',['../class_adafruit___t_m_f8806.html#aea5275982a2c4052bc2d4f10299ddee8',1,'Adafruit_TMF8806']]],
+  ['getgpiomode',['getGPIOMode',['../class_adafruit___t_m_f8806.html#ae89622dbb67499d49db3a67f2991d00c',1,'Adafruit_TMF8806']]],
+  ['getiterations',['getIterations',['../class_adafruit___t_m_f8806.html#aa0c3527c3d6fcac593af8015e5d0eed8',1,'Adafruit_TMF8806']]],
+  ['getopticalconfig',['getOpticalConfig',['../class_adafruit___t_m_f8806.html#a8559d41e8eacaaec130401d306a24018',1,'Adafruit_TMF8806']]],
+  ['getrepetitionperiod',['getRepetitionPeriod',['../class_adafruit___t_m_f8806.html#a26cf694a2011df4157ae0d3eb40ceea5',1,'Adafruit_TMF8806']]],
   ['getrevisionid',['getRevisionID',['../class_adafruit___t_m_f8806.html#ae5f76259406577a6d655edf07a855373',1,'Adafruit_TMF8806']]],
+  ['getspaddeadtime',['getSpadDeadTime',['../class_adafruit___t_m_f8806.html#ac6b83134c799eac3fe3e546650589d90',1,'Adafruit_TMF8806']]],
   ['gettemperature',['getTemperature',['../class_adafruit___t_m_f8806.html#a455f7ad2705c87fbf31e692e8034b8af',1,'Adafruit_TMF8806']]],
+  ['getthreshold',['getThreshold',['../class_adafruit___t_m_f8806.html#a983d81374bc36c9d040a04c8e5cf4b45',1,'Adafruit_TMF8806']]],
   ['getversion',['getVersion',['../class_adafruit___t_m_f8806.html#ac2ea884819489c036eeb0fabe758f360',1,'Adafruit_TMF8806']]]
 ];
